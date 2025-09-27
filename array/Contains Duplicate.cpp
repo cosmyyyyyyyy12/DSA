2ce80efc1d1ee43a87d39dd25549ef35b75a5b1e
Tanishk <tanishkthegreat13@gmail.com>
2025-09-27 15:13:44 +0530

    Add files via upload

---
+++ array/Contains Duplicate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main(){
+    int nums[4] = {1,2,3,1};
+    unordered_map <int,int> count;
+    for(int i=0;i<4;i++){
+        count[nums[i]]++;
+    }
+    for(auto &i:count){
+        if(i.second==2){
+            cout << "true";
+        }
+    }
+}
