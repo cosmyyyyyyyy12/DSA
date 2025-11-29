16ee7a7684615c0860107b600f7686f13c0a6027
Tanishk <tanishkthegreat13@gmail.com>
2025-11-29 12:07:30 +0530

    Add files via upload

---
+++ string/Length of Last Word.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+        int count = 0;
+        while (i >= 0 && s[i] != ' ') {
+            count++;
+            i--;
+        }
+        return count;
+    }
+};
+
+int main(){
+    Solution s;
+    cout << s.lengthOfLastWord("Hello World") << "\n";
+}
