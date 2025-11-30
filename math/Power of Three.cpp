b171fc818b9d78a7f0b3c64eb77a8aa79c022e50
Tanishk <tanishkthegreat13@gmail.com>
2025-11-30 20:01:40 +0530

    Add files via upload

---
+++ math/Power of Three.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0){
+            return false;
+        }
+        while(n % 3 == 0){
+            n/=3;
+        }
+        return n == 1;
+    }
+};
+
+int main(){
+    Solution s;
+    cout << s.isPowerOfThree(27) << "\n";
+}
