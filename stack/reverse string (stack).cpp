1b7eb8a4fa83db916026867238c83cea61e7fe6e
Tanishk <tanishkthegreat13@gmail.com>
2026-01-01 22:33:00 +0530

    Add files via upload

---
+++ stack/reverse string (stack).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(){
+    string s = "hello";
+    stack <char> st;
+    for(char c : s){
+        st.push(c);
+    }
+    while(!st.empty()){
+        cout << st.top();
+        st.pop();
+    }
+}
