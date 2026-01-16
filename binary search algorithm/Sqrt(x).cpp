7fb768c24e25e9f07faa94cc7be4b622c7611c9f
Tanishk <tanishkthegreat13@gmail.com>
2026-01-16 09:41:55 +0530

    Create Sqrt(x).cpp

---
+++ binary search algorithm/Sqrt(x).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long square = (long long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } 
+            else if (square < x) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+};
+
+int main(){
+    Solution sol;
+    int x = 4;
+    cout << sol.mySqrt(x);
+}
