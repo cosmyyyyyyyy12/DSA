07a6c4bdaba091e6fd44ec5bb9fb813bc0137566
Tanishk <tanishkthegreat13@gmail.com>
2026-02-20 13:50:00 +0530

    Add files via upload

---
+++ array/Search Insert Position.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size() - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+
+        return s; // Insert position
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {1, 3, 5, 6};
+    int target = 2;
+
+    cout << "Insert Position: "
+         << sol.searchInsert(nums, target);
+
+    return 0;
+}
